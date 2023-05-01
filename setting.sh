52ea0bfb3fda8733ed0416ea4b377ab5cbd87cb8
simonlee91 <smion@provalidator.com>
2023-05-01 16:00:30 +0900

    v2.0.0

--- setting.sh
+++ setting.sh
@@ -1,185 +1,29 @@
 #!/bin/bash
 
 chmod u+r+x ./config/config.sh
-sources ./config/config.sh
 
-# Change settings Configuration
-GO_VERSION=1.18.10
-SNAP_SHOT_URL={snapshot_url}
-EXTERNAL_ADDRESS="{server_ip}:26656"
+COSMOS=1
+JUNO=2
+SIFCHIAN=3
+EVMOS=4
 
-# Basic Configuration
-MEMPOOL_SIZE=500
-PRUNING="custom"
-PRUNING_KEEP_RECENT="100"
-PRUNING_KEEP_EVERY="2000"
-PRUNING_INTERVAL="10"
-CHECK=1
-
-# Basic Installation
-echo "Installing Basic..."
-sudo apt-get update  
-sudo apt-get upgrade -y
-sudo apt-get install build-essential make jq net-tools gcc gzip sysstat htop chrony manpages-dev -y
-sudo apt install make clang pkg-config libssl-dev build-essential git jq ncdu bsdmainutils htop net-tools lsof -y < "/dev/null"
-sudo apt update
-sudo apt install snapd -y
-sudo snap install lz4
-
-# Install nodejs
-echo "Installing node.js..."
-curl https://deb.nodesource.com/setup_20.x | sudo bash
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt install nodejs=20.* yarn build-essential jq -y
-
-# Install go $GO_VERSION
-echo "Installing go..."
-rm go*linux-amd64.tar.gz
-wget https://go.dev/dl/go$GO_VERSION.linux-amd64.tar.gz
-sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go$GO_VERSION.linux-amd64.tar.gz
-
-# Update environment variables to include go
-cat <<'EOF' >>$HOME/.profile
-export GOROOT=/usr/local/go
-export GOPATH=$HOME/go
-export GO111MODULE=on
-export GOBIN=$GOPATH/bin
-export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
-EOF
-. $HOME/.profile
-mkdir -p ~/go/bin | mkdir -p ~/go/src/github.com | mkdir -p ~/go/pkg
-
-# Install $CHAIN_BINARY binary
-echo "Installing $CHAIN_NAME..."
-cd $HOME
-cd ~/go/src/github.com
-git clone $GITURL
-cd $CHAIN_NAME
-git checkout $BRANCH
-make install
-
-# Initialize home directory
-echo "Initializing $NODE_HOME..."
-rm -rf $NODE_HOME
-$CHAIN_BINARY init $NODE_MONIKER --chain-id $CHAIN_ID
-sed -i -e "/seeds =/ s^= .*^= \"$SEEDS\"^" $NODE_HOME/config/config.toml
-sed -i -e "/persistent_peers =/ s^= .*^= \"$PERSISTENT_PEERS\"^" $NODE_HOME/config/config.toml
-sed -i -e "/external_address =/ s^= .*^= \"$EXTERNAL_ADDRESS\"^" $NODE_HOME/config/config.toml
-sed -i -e "/^size =/ s^= .*^= $MEMPOOL_SIZE^" $NODE_HOME/config/config.toml
-sed -i -e "/minimum-gas-prices =/ s^= .*^= \"$MINIMUM_GAS_PRICES\"^" $NODE_HOME/config/app.toml
-sed -i -e "/pruning =/ s^= .*^= \"$PRUNING\"^" $NODE_HOME/config/app.toml
-sed -i -e "/pruning-keep-recent =/ s^= .*^= \"$PRUNING_KEEP_RECENT\"^" $NODE_HOME/config/app.toml
-sed -i -e "/pruning-keep-every =/ s^= .*^= \"$PRUNING_KEEP_EVERY\"^" $NODE_HOME/config/app.toml
-sed -i -e "/pruning-interval =/ s^= .*^= \"$PRUNING_INTERVAL\"^" $NODE_HOME/config/app.toml
-sed -i -e "/chain-id =/ s^= .*^= \"$CHAIN_ID\"^" $NODE_HOME/config/client.toml
-
-# Replace genesis file: only after the spawn time is reached
-echo "Replacing genesis file..."
-wget -O genesis.json $GENESIS_URL --inet4-only
-mv genesis.json $NODE_HOME/config/genesis.json
-
-if $STATE_SYNC ; then
-    echo "Configuring state sync..."
-    CURRENT_BLOCK=$(curl -s $SYNC_RPC_1/block | jq -r '.result.block.header.height')
-    TRUST_HEIGHT=$[$CURRENT_BLOCK-1000]
-    TRUST_BLOCK=$(curl -s $SYNC_RPC_1/block\?height\=$TRUST_HEIGHT)
-    TRUST_HASH=$(echo $TRUST_BLOCK | jq -r '.result.block_id.hash')
-    sed -i -e '/enable =/ s/= .*/= true/' $NODE_HOME/config/config.toml
-    sed -i -e "/trust_height =/ s/= .*/= $TRUST_HEIGHT/" $NODE_HOME/config/config.toml
-    sed -i -e "/trust_hash =/ s/= .*/= \"$TRUST_HASH\"/" $NODE_HOME/config/config.toml
-    sed -i -e "/rpc_servers =/ s^= .*^= \"$SYNC_RPC_SERVERS\"^" $NODE_HOME/config/config.toml
-else
-    echo "Skipping state sync..."
-fi
-
-if $SNAP_SHOT ; then
-    echo "Setting snap shot..."
-    wget -O snap_shot.tar.lz4 $SNAP_SHOT_URL
-    lz4 -c -d snap_shot.tar.lz4  | tar -x -C $NODE_HOME
-    rm -v snap_shot.tar.lz4
-else
-    echo "Skipping snap shot..."
-fi
-
-echo "Cosmovisor Install?"
-echo "1. Yes"
-echo "2. No"
+# Select Chain
+echo "What network you going to interact:"
+echo "1. CosmosHub"
+echo "2. Juno"
+echo "3. Sifchian"
+echo "4. Evmos"
 
 read i
 
-if [ {$i} == {$CHECK} ]; then
-
-# Set up cosmovisor
-SERVICE_NAME=cosmovisor
-echo "Setting up cosmovisor..."
-mkdir -p $NODE_HOME/cosmovisor/genesis/bin
-mkdir -p $NODE_HOME/cosmovisor/upgrades
-cp $(which $CHAIN_BINARY) $NODE_HOME/cosmovisor/genesis/bin
-
-echo "Installing cosmovisor..."
-export BINARY=$NODE_HOME/cosmovisor/genesis/bin/$CHAIN_BINARY
-go install github.com/cosmos/cosmos-sdk/cosmovisor/cmd/cosmovisor@v1.3.0
-
-echo "Creating $SERVICE_NAME.service..."
-sudo rm /etc/systemd/system/$SERVICE_NAME.service
-sudo tee <<EOF >/dev/null /etc/systemd/system/$SERVICE_NAME.service
-
-[Unit]
-Description=Cosmovisor service
-After=network-online.target
-
-[Service]
-User=$USER
-ExecStart=$HOME/go/bin/cosmovisor run start --x-crisis-skip-assert-invariants
-Restart=always
-RestartSec=10
-LimitNOFILE=50000
-Environment='DAEMON_NAME=$CHAIN_BINARY'
-Environment='DAEMON_HOME=$NODE_HOME'
-Environment='DAEMON_ALLOW_DOWNLOAD_BINARIES=true'
-Environment='DAEMON_RESTART_AFTER_UPGRADE=true'
-Environment='DAEMON_LOG_BUFFER_SIZE=512'
-Environment='UNSAFE_SKIP_BACKUP=true'
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-# Start service
-sudo systemctl daemon-reload
-
-# Enable and start
-sudo systemctl enable $SERVICE_NAME.service
-sudo systemctl start $SERVICE_NAME.service
-sudo systemctl restart systemd-journald
-
+if [ {$i} == {$COSMOS} ]; then
+    bash ./chians/cosmos_setting.sh
+elif [ {$i} == {$JUNO} ]; then
+    bash ./chians/juno_setting.sh
+elif [ {$i} == {$SIFCHIAN} ]; then
+    bash ./chians/sifchian_setting.sh
+elif [ {$i} == {$EVMOS} ]; then
+    bash ./chians/evmos_setting.sh
 else
-# Set up Daemon
-echo "Creating $CHAIN_BINARY.service..."
-sudo rm /etc/systemd/system/$CHAIN_BINARY.service
-
-sudo tee <<EOF >/dev/null /etc/systemd/system/$CHAIN_BINARY.service
-[Unit]
-Description=$CHAIN_BINARY daemon
-After=network-online.target
-
-[Service]
-User=$USER
-ExecStart=$HOME/go/bin/$CHAIN_BINARY start
-Restart=on-failure
-RestartSec=10
-LimitNOFILE=10000
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-# Start service
-sudo systemctl daemon-reload
-
-# Enable and start the service
-sudo systemctl enable $CHAIN_BINARY.service
-sudo systemctl start $CHAIN_BINARY.service
-sudo systemctl restart systemd-journald
+    echo "It's a chain that doesn't exist."
 fi
