4950a788dcdc94d313d1790e74f1590c83870aa1
simonlee91 <smion@provalidator.com>
2023-06-21 09:46:59 +0900

    zetachain

---
+++ chians/zetachain_setting.sh
@@ -0,0 +1,193 @@
+#!/bin/bash
+# Set up a service to join the  chain.
+
+source ./config/config.sh
+
+# Configuration
+BRANCH=pismoC
+NODE_HOME=$HOME/.agoric
+NODE_MONIKER=validator
+GITURL=https://github.com/Agoric/agoric-sdk
+CHAIN_NAME=athens_7001-1
+CHAIN_BINARY=agd
+CHAIN_ID=agoric-3
+SNAP_SHOT_URL=https://snapshots.polkachu.com/snapshots/agoric/agoric_10079474.tar.lz4
+SEEDS=""
+PERSISTENT_PEERS="9ed68bef54712b46713ac755ab7a6e7ad30694ef@192.99.44.79:14456,506f9bca6ce2f29a2556427f90693a8ee1b100ff@178.128.238.183:26060,abc62ded9142361bd9832282242a53611785ffcd@51.81.109.109:26656,98d989f486d42ec75203f918495c420ca9665514@34.122.28.103:26656,f095bb53006ebddcbbf29c8df70dddcba6419e36@142.93.145.13:26656,aede0d57cd77051cf1270675fa770c22e8074501@64.32.40.134:26656,4bc6c457c018b81a19efa49a9e403b64535decf1@137.184.141.111:26656"
+SYNC_RPC_1=https://agoric-rpc.polkachu.com:443
+SYNC_RPC_SERVERS="$SYNC_RPC_1,$SYNC_RPC_1"
+GENESIS_URL=https://snapshots.polkachu.com/genesis/agoric/genesis.json
+MINIMUM_GAS_PRICES="0ubld"
+CHECK=1
+
+# Basic Installation
+echo "Installing Basic..."
+sudo apt-get update  
+sudo apt-get upgrade -y
+sudo apt-get install build-essential make jq net-tools gcc gzip sysstat htop chrony manpages-dev -y
+sudo apt install make clang pkg-config libssl-dev build-essential git jq ncdu bsdmainutils htop net-tools lsof -y < "/dev/null"
+sudo apt update
+sudo apt install snapd -y
+sudo snap install lz4
+
+# Install nodejs
+echo "Installing node.js..."
+curl https://deb.nodesource.com/setup_18.x | sudo bash
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt install nodejs=18.* yarn build-essential jq -y
+
+# Install yran
+npm install -g yarn
+
+# Install go $GO_VERSION
+echo "Installing go..."
+rm go*linux-amd64.tar.gz
+wget https://go.dev/dl/go$GO_VERSION.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go$GO_VERSION.linux-amd64.tar.gz
+
+# Update environment variables to include go
+cat <<'EOF' >>$HOME/.profile
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
+export GO111MODULE=on
+export GOBIN=$GOPATH/bin
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+EOF
+source $HOME/.profile
+mkdir -p ~/go/bin | mkdir -p ~/go/src/github.com | mkdir -p ~/go/pkg
+
+# Install $CHAIN_BINARY binary
+echo "Installing $CHAIN_NAME..."
+cd $HOME
+cd ~/go/src/github.com
+git clone $GITURL
+cd $CHAIN_NAME
+git checkout $BRANCH
+yarn install
+yarn build
+cd packages/cosmic-swingset && make
+
+# Initialize home directory
+echo "Initializing $NODE_HOME..."
+rm -rf $NODE_HOME
+$CHAIN_BINARY init $NODE_MONIKER --chain-id $CHAIN_ID
+sed -i -e "/seeds =/ s^= .*^= \"$SEEDS\"^" $NODE_HOME/config/config.toml
+sed -i -e "/persistent_peers =/ s^= .*^= \"$PERSISTENT_PEERS\"^" $NODE_HOME/config/config.toml
+sed -i -e "/external_address =/ s^= .*^= \"$EXTERNAL_ADDRESS\"^" $NODE_HOME/config/config.toml
+sed -i -e "/^size =/ s^= .*^= $MEMPOOL_SIZE^" $NODE_HOME/config/config.toml
+sed -i -e "/minimum-gas-prices =/ s^= .*^= \"$MINIMUM_GAS_PRICES\"^" $NODE_HOME/config/app.toml
+sed -i -e "/pruning =/ s^= .*^= \"$PRUNING\"^" $NODE_HOME/config/app.toml
+sed -i -e "/pruning-keep-recent =/ s^= .*^= \"$PRUNING_KEEP_RECENT\"^" $NODE_HOME/config/app.toml
+sed -i -e "/pruning-interval =/ s^= .*^= \"$PRUNING_INTERVAL\"^" $NODE_HOME/config/app.toml
+sed -i -e "/chain-id =/ s^= .*^= \"$CHAIN_ID\"^" $NODE_HOME/config/client.toml
+
+# Replace genesis file: only after the spawn time is reached
+echo "Replacing genesis file..."
+wget -O genesis.json $GENESIS_URL --inet4-only
+mv genesis.json $NODE_HOME/config/genesis.json
+
+if $STATE_SYNC ; then
+    echo "Configuring state sync..."
+    CURRENT_BLOCK=$(curl -s $SYNC_RPC_1/block | jq -r '.result.block.header.height')
+    TRUST_HEIGHT=$[$CURRENT_BLOCK-1000]
+    TRUST_BLOCK=$(curl -s $SYNC_RPC_1/block\?height\=$TRUST_HEIGHT)
+    TRUST_HASH=$(echo $TRUST_BLOCK | jq -r '.result.block_id.hash')
+    sed -i -e '/enable =/ s/= .*/= true/' $NODE_HOME/config/config.toml
+    sed -i -e "/trust_height =/ s/= .*/= $TRUST_HEIGHT/" $NODE_HOME/config/config.toml
+    sed -i -e "/trust_hash =/ s/= .*/= \"$TRUST_HASH\"/" $NODE_HOME/config/config.toml
+    sed -i -e "/rpc_servers =/ s^= .*^= \"$SYNC_RPC_SERVERS\"^" $NODE_HOME/config/config.toml
+else
+    echo "Skipping state sync..."
+fi
+
+if $SNAP_SHOT ; then
+    echo "Setting snap shot..."
+    wget -O snap_shot.tar.lz4 $SNAP_SHOT_URL
+    lz4 -c -d snap_shot.tar.lz4  | tar -x -C $NODE_HOME
+    rm -v snap_shot.tar.lz4
+else
+    echo "Skipping snap shot..."
+fi
+
+echo "Cosmovisor Install?"
+echo "1. Yes"
+echo "2. No"
+
+read i
+
+if [ {$i} == {$CHECK} ]; then
+
+# Set up cosmovisor
+SERVICE_NAME=cosmovisor
+echo "Setting up cosmovisor..."
+mkdir -p $NODE_HOME/cosmovisor/genesis/bin
+mkdir -p $NODE_HOME/cosmovisor/upgrades
+ln -s $HOME/go/src/github.com/agoric-sdk/bin/agd $NODE_HOME/cosmovisor/genesis/bin/agd
+
+echo "Installing cosmovisor..."
+export BINARY=$NODE_HOME/cosmovisor/genesis/bin/$CHAIN_BINARY
+go install github.com/cosmos/cosmos-sdk/cosmovisor/cmd/cosmovisor@v1.3.0
+
+echo "Creating $SERVICE_NAME.service..."
+sudo rm /etc/systemd/system/$SERVICE_NAME.service
+sudo tee <<EOF >/dev/null /etc/systemd/system/$SERVICE_NAME.service
+
+[Unit]
+Description=Cosmovisor service
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$HOME/go/bin/cosmovisor run start --x-crisis-skip-assert-invariants
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=50000
+Environment='DAEMON_NAME=$CHAIN_BINARY'
+Environment='DAEMON_HOME=$NODE_HOME'
+Environment='DAEMON_ALLOW_DOWNLOAD_BINARIES=false'
+Environment='DAEMON_RESTART_AFTER_UPGRADE=true'
+Environment='DAEMON_LOG_BUFFER_SIZE=512'
+Environment='UNSAFE_SKIP_BACKUP=true'
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Start service
+sudo systemctl daemon-reload
+
+# Enable and start
+sudo systemctl enable $SERVICE_NAME.service
+sudo systemctl start $SERVICE_NAME.service
+sudo systemctl restart systemd-journald
+
+else
+# Set up Daemon
+echo "Creating $CHAIN_BINARY.service..."
+sudo rm /etc/systemd/system/$CHAIN_BINARY.service
+
+sudo tee <<EOF >/dev/null /etc/systemd/system/$CHAIN_BINARY.service
+[Unit]
+Description=$CHAIN_BINARY daemon
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$HOME/go/bin/$CHAIN_BINARY start
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=10000
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Start service
+sudo systemctl daemon-reload
+
+# Enable and start the service
+sudo systemctl enable $CHAIN_BINARY.service
+sudo systemctl start $CHAIN_BINARY.service
+sudo systemctl restart systemd-journald
+fi
